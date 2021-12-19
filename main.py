6ae169371ece30b205e4cac2e448cfaad2ee389b
Zero-i00 <mrshelkinpro228@gmail.com>
2021-12-19 16:33:07 +0500

    Politika Confiditional

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ purple = [1, 0, 1, 1]
 class MyApp(App):
     def build(self):
         screenmenu = Screen(name='Menu')
-        screenmenu1 = Screen(name='scr2')
+        screenmenu1 = Screen(name='Wiki')
+        screenmenu2 = Screen(name='scr3')
         self.sm = ScreenManager(transition=SlideTransition())
         self.sm.add_widget(screenmenu)
         self.sm.add_widget(screenmenu1)
@@ -23,14 +24,32 @@ class MyApp(App):
         img = Image(source='hahaha.jpg')
         txt = Label(text = 'Привет я бабушка Олег! Начни работу, а то съем.')
         button = Button(text = 'Начать Работу!',
-                    background_color = random.choice(colors))
+                        background_color=random.choice(colors))
         button.bind(on_press=self.on_press_button)
         hl.add_widget(img)
         hl.add_widget(txt)
         hl.add_widget(button)
+        hl1 = BoxLayout(orientation="vertical")
+        screenmenu1.add_widget(hl1)
+        img = Image(source='med.jpg')
+        txt = Label(text='Народная медицина – часть альтернативной медицины,методах и средствах лечения,'
+                         ' которые передаются в народе из поколения в поколение.Но также народная медицина может'
+                         ' включать элементы магии, эзотерики и «альтернативные», часто неэффективные, или даже опасные'
+                         ' методы лечения.Поэтому читая эти статьи вы принимаете то что вы сами отвичаете за свою '
+                         'жизнь', text_size=(300, None))
+        button = Button(text = 'Я соглашаюсь с правилами которые придумал разработчик ,но они не где не '
+                               'прописаны',
+                        text_size=(200, None),
+                        background_color = random.choice(colors))
+        #button.bind(on_press=self.on_press_button)
+        hl1.add_widget(img)
+        hl1.add_widget(txt)
+        hl1.add_widget(button)
         return self.sm
     def on_press_button(self, instance):
-        self.sm.current = 'scr2'
+        self.sm.current = 'Wiki'
+    #def on_press_button(self, instance):
+        #self.sm.current = 'scr3'
 Window.size = (450 , 935)
 app=MyApp()
 app.run()
